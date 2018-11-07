4699dee7466ec863a4555fc629c7622e08111972
JustinTheeDude <JustinTheeDude@gmail.com>
2018-11-06 19:55:08 -0800

    added yelp api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        restaraunt: []
+    };
+
+    componentDidMount() {
+        axios.defaults.headers.common['Authorization'] = process.env.REACT_APP_API_KEY;
+        axios.get('https://api.yelp.com/v3/businesses/search')
+            .then(res => console.log(res.name));
+    };
+
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            </header>
+            </div>
+        );
+    }
 }
 
 export default App;
