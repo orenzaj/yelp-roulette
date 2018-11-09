4c72965b99fa000fdfc8de1d359f19897476c569
JustinTheeDude <JustinTheeDude@gmail.com>
2018-11-08 20:58:43 -0800

    oops lul

--- nodeserver.js
+++ nodeserver.js
@@ -1,9 +1,10 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3001;
 const yelp = require('yelp-fusion');
-const apiKey = 'QteV0iU5d1seg7X2tBC_McX2Z89Z9HEhM_NaRQRbaeCO5JT-DbGdxLRwmQ5nEoL8j7_OmNAcIMWq_jB6ldk4Qyf5YsWKGL0voLX-YAmRxGuocfzGHccP0wPSrh7dW3Yx'
+const apiKey = process.env.REACT_APP_API_KEY;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
